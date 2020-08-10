01351fc374f5067c990e8c3c8239664e2b64f489
parvanitakis <parvanitakis@innovative.gr>
2020-08-10 16:41:22 +0300

    app

---
+++ project-api/src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const userRoutes = require('../routes/userRoutes');
+const cors = require('cors');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+app.use("/user", userRoutes);
+// error handler
+app.use(function (err, req, res, next) {
+    try {
+        res.status(err.status || 500);
+
+        res.json({
+            status: err.status,
+            type: err.message
+        });
+        next();
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+module.exports = app;
