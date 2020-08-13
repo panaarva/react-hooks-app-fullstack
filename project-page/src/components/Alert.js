02d0e93cefc6a8dcc00d47594b662c5bb8e0de6a
parvanitakis <parvanitakis@innovative.gr>
2020-08-13 12:55:35 +0300

    alert component

---
+++ project-page/src/components/Alert.js
@@ -0,0 +1,18 @@
+import React, from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default function ({alert, setAlert}) {
+    const {severity, message, openAlert} = alert;
+    return (
+        <Snackbar open={openAlert} autoHideDuration={6000} onClose={() => setAlert({...alert, openAlert: false})}>
+            <Alert onClose={() => setAlert({...alert, openAlert: false})} severity={severity}>
+                {message}
+            </Alert>
+        </Snackbar>
+    )
+}
