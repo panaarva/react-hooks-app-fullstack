b3fc41183012a3a18ed4411e7f4d8a922772a152
parvanitakis <parvanitakis@innovative.gr>
2020-08-14 14:24:50 +0300

    utils

--- project-api/lib/utils.js
+++ project-api/lib/utils.js
@@ -4,21 +4,18 @@ const decode = (authCode) => {
     let privateKey = `kn0wled9e#1998`;
     let userInfo;
     try {
-        userInfo = jwt.verify(authCode,privateKey);
-    }catch (e){
+        userInfo = jwt.verify(authCode, privateKey);
+    } catch (e) {
         userInfo = null;
         console.error(e);
     }
     return userInfo;
 }
-const incode = (response) =>{
-    const rows = {rows: response.rows};
-    console.log(`{\"rows\":${rows}}`);
-    let authCode = null;
-    if(response.rows.length > 0) {
-        let privateKey = `kn0wled9e#1998`;
-        authCode = jwt.sign(rows, privateKey);
-    }
+const incode = (response) => {
+    const rows = {rows: (response.rows) ? response.rows : {}};
+    let authCode;
+    let privateKey = `kn0wled9e#1998`;
+    authCode = jwt.sign(rows, privateKey);
     return authCode;
 }
 module.exports = {
