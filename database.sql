011d964b5baf323323443487fe2698b806538e90
parvanitakis <parvanitakis@innovative.gr>
2020-08-24 15:00:07 +0300

    database

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public."user" (
+	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	username text NOT NULL,
+	userpassword text NOT NULL,
+	email text NOT NULL,
+	gender text NOT NULL,
+	bornday timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT user_pkey PRIMARY KEY (id)
+);
