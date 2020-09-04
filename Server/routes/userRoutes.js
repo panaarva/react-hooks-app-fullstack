eb27dac3948924d252ffd9957f3d728aa5280503
parvanitakis <parvanitakis@innovative.gr>
2020-09-04 14:40:33 +0300

    useRoutes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+    const router = express.Router();
 const createError = require('http-errors');
 const client = require('../lib/config').pool;
 const {encode, decode} = require('../lib/utils');
