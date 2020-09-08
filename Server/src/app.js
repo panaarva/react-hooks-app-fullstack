f93806de5347ee63eceeecb1b13823d240ae4093
parvanitakis <parvanitakis@innovative.gr>
2020-09-08 14:29:41 +0300

    app

--- Server/src/app.js
+++ Server/src/app.js
@@ -5,15 +5,17 @@ const userRoutes = require('../routes/userRoutes');
 const cors = require('cors');
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../../Client/build')));
+    app.use(express.static(path.join(__dirname, 'Client/build')));
 }
+console.log(path.join(__dirname, 'Client/build'));
+console.log(path.join(__dirname, '../../Client/build'));
 app.use(express.static(path.join(__dirname, "../../Client/build")));
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
 app.use("/", userRoutes);
 app.get('*',async (req,res)=>{
-    res.sendFile(path.join(__dirname, "../../Client/build/index.html"));
+    res.sendFile(path.join(__dirname, "Client/build/index.html"));
 })
 // error handler
 app.use(function (err, req, res, next) {
